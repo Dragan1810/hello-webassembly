82c00d7859b7d05addd3b1d1811d730c443ec373
Dragan <d.novakovic1810@yahoo.com>
2018-07-14 18:58:03 +0200

    scrpit and index

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+pub fn parse(input: &str) -> String {
+    let result = parser::parse(input.to_string());
+    result
+}
+
 /*
 #[wasm_bindgen]
 extern "C" {
